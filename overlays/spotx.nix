36f507e2f36e52977b5ab9651cc1a4f35f0ff5ee
somebodykot <92354102+somebodykot@users.noreply.github.com>
2026-06-29 15:41:35 +0400

    Update spotx.nix

--- overlays/spotx.nix
+++ overlays/spotx.nix
@@ -1,45 +1,29 @@
 final: prev:
 let
+  old-pkgs = import (prev.fetchzip {
+    url = "https://github.com/NixOS/nixpkgs/archive/release-23.05.tar.gz";
+    hash = "sha256-mlUc62b7Mw+Rpuiy0kqov5JCfrvKqgHLYfTf221gerM=";  # ①
+  }) { system = prev.system; };
+
   spotx = prev.fetchurl {
     url = "https://github.com/SpotX-Official/SpotX-Bash/raw/5e9b08f91e55c210bbc64715b4ad698186b3c06b/spotx.sh";
     hash = "sha256-mlUc62b7Mw+Rpuiy0kqov5JCfrvKqgHLYfTf221gerM=";
   };
 in
 {
-  spotify = prev.spotify.overrideAttrs (old: {
+  spotify = old-pkgs.spotify.overrideAttrs (old: {
     nativeBuildInputs =
       old.nativeBuildInputs
-      ++ (with prev; [
-        util-linux
-        perl
-        unzip
-        zip
-        curl
-      ]);
+      ++ (with prev; [ util-linux perl unzip zip curl ]);
 
-    unpackPhase =
-      builtins.replaceStrings
-        [ "runHook postUnpack" ]
-        [
-          ''
-            patchShebangs --build ${spotx}
-            runHook postUnpack
-          ''
-        ]
-        old.unpackPhase;
+    unpackPhase = ''
+      ${old.unpackPhase}
+      patchShebangs --build ${spotx}
+    '';
 
-    installPhase =
-      builtins.replaceStrings
-        [ "runHook postInstall" ]
-        [
-          ''
-            bash ${spotx} \
-            -f \
-            -P "$out/share/spotify" \
-            -o on \
-            runHook postInstall
-          ''
-        ]
-        old.installPhase;
+    installPhase = ''
+      bash ${spotx} -f -P "$out/share/spotify" -o on
+      ${old.installPhase}
+    '';
   });
 }
